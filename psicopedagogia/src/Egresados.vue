1ea94384822e2835c15e61aa73aa4460e94d3570
inaDAGod <danialee14@gmail.com>
2024-04-16 23:41:11 -0400

    add egresados carrusel

--- psicopedagogia/src/Egresados.vue
+++ psicopedagogia/src/Egresados.vue
@@ -1,52 +1,63 @@
 <template>
-    <div>
-      <h1>NUESTROS EGRESADOS</h1>
-      <ul>
-      <li v-for="(egresado, index) in egresados" :key="egresado.id_egresado">
-        <img :src="egresado.src_foto"  style="width: 100px; height: 100px;">
-        <div>
-          <strong>{{ egresado.nombre }}</strong> - {{ egresado.trabajo }}
+    <h2>NUESTROS EGRESADOS</h2>
+    <div class="carousel">
+      <div class="carousel-inner">
+        <div v-for="(egresado, index) in visibleIndices" :key="index" class="carousel-item">
+          <div class="img-container">
+            <img :src="egresados[egresado].src_foto" :class="{ 'img-oval': true, 'blur': egresado !== currentIndex, 'focused': egresado === currentIndex }" alt="...">
+          </div>
+          <div class="carousel-caption d-none d-md-block">
+            <h3 style="color: black;">{{ egresados[egresado].nombre }}</h3>
+          </div>
         </div>
-      </li>
-    </ul>
+      </div>
+      <button class="carousel-control-prev" type="button" @click="prevSlide">
+        <
+      </button>
+      <button class="carousel-control-next" type="button" @click="nextSlide">
+        >
+      </button>
     </div>
   </template>
   
-  
   <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
-
-const egresados = ref([]);
-const currentIndex = ref(0);
-
-onMounted(async () => {
-  try {
-    const response = await axios.get('http://localhost:3000/api/egresados');
-    egresados.value = response.data;
-  } catch (error) {
-    console.error('Error al obtener los egresados:', error);
-  }
-});
-
-const nextSlide = () => {
-  if (currentIndex.value < egresados.value.length - 1) {
-    currentIndex.value++;
-  }
-};
-
-const prevSlide = () => {
-  if (currentIndex.value > 0) {
-    currentIndex.value--;
-  }
-};
-</script>
-
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
+  
+  const egresados = ref([]);
+  const currentIndex = ref(0);
+  const visibleIndices = ref([]);
+  
+  onMounted(async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/egresados');
+      egresados.value = response.data;
+      updateVisibleIndices();
+    } catch (error) {
+      console.error('Error al obtener los egresados:', error);
+    }
+  });
+  
+  const nextSlide = () => {
+    currentIndex.value = (currentIndex.value + 1) % egresados.value.length;
+    updateVisibleIndices();
+  };
+  
+  const prevSlide = () => {
+    currentIndex.value = (currentIndex.value - 1 + egresados.value.length) % egresados.value.length;
+    updateVisibleIndices();
+  };
+  
+  const updateVisibleIndices = () => {
+    const totalIndices = egresados.value.length;
+    const prevIndex = (currentIndex.value - 1 + totalIndices) % totalIndices;
+    const nextIndex = (currentIndex.value + 1) % totalIndices;
+    visibleIndices.value = [prevIndex, currentIndex.value, nextIndex];
+  };
+  
+  </script>
   
   <style>
-    @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
-    @import url('/src/assets/styleD.css');
-    .carrusel{
-        background-color: #FEF684;
-    }
+    @import url('/src/assets/egresados.css');
   </style>
+  
