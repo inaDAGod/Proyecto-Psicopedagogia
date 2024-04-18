4e84bc314157fcf61f23257117a09c7a53480694
unknown <ayana.siegle@ucb.edu.bo>
2024-04-18 00:33:40 -0400

    Main.js cambiar de prinicpal a nosotros
    solo lo comente

--- psicopedagogia/src/main.js
+++ psicopedagogia/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
-//import App from './Egresados.vue' //import App2 from './principal.vue'
+//import App from './Egresados.vue' //
+//import App2 from './principal.vue'
 import App from './Nosotros.vue'
 createApp(App).mount('#app')
