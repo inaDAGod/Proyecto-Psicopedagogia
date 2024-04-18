700d37b77a80d653a5ae161db4e355684fd88415
unknown <ayana.siegle@ucb.edu.bo>
2024-04-17 22:51:10 -0400

    Update main.js

--- psicopedagogia/src/main.js
+++ psicopedagogia/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './Egresados.vue' //aca cambian su nombre de su bue para probar
-
+//import App from './Egresados.vue' //aca cambian su nombre de su bue para probar
+import App from './Nosotros.vue'
 createApp(App).mount('#app')
