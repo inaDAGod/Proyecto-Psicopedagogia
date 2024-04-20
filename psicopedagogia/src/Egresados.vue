e7ed1fddf8d1f288ea75bcd19910bf103c660893
inaDAGod <danialee14@gmail.com>
2024-04-20 11:04:52 -0400

    update egresados and add egresadosAdmi

--- psicopedagogia/src/Egresados.vue
+++ psicopedagogia/src/Egresados.vue
@@ -2,28 +2,14 @@
   <div class="cuerpo">
     <h2>NUESTROS EGRESADOS</h2>
     <div>
-      <Carrusel :egresados="egresados" :currentIndex="currentIndex" :visibleIndices="visibleIndices" />
+      <Carrusel/>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
+
 import Carrusel from './components/EgresadosCarrusel.vue';
-
-const egresados = ref([]);
-const currentIndex = ref(0);
-const visibleIndices = ref([]);
-
-onMounted(async () => {
-  try {
-    const response = await axios.get('http://localhost:3000/api/egresados');
-    egresados.value = response.data;
-  } catch (error) {
-    console.error('Error al obtener los egresados:', error);
-  }
-});
 </script>
 
 <style scoped>
