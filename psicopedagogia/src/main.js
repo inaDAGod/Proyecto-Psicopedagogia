879d44b9376418a2d413777a3afe76e8d3e24cdf
unknown <ayana.siegle@ucb.edu.bo>
2024-04-21 16:03:38 -0400

    nosotros admin frontend
    no esta funcional aun

--- psicopedagogia/src/main.js
+++ psicopedagogia/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 //import App from './Egresados.vue' //
 //import App2 from './principal.vue'
-import App from './Nosotros.vue'
+//import App from './Nosotros.vue'
+import App from './Nosotrosadmin.vue'
 createApp(App).mount('#app')
