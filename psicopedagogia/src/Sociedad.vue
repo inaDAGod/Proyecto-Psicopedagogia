813945f1f52da1ff8e8f82ae031dd8788897b867
inaDAGod <danialee14@gmail.com>
2024-04-23 14:41:53 -0400

    add sociedad.vue y title with koulen title

---
+++ psicopedagogia/src/Sociedad.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2 class="titleInves">Investigacion <br>Sociedad Científica Estudiantil INPSICOPEDIA</h2>
+</template>
+
+<script>
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Koulen&display=swap');
+.titleInves{
+    color: #FF7001;
+    font-family: 'Koulen';
+    font-size: 300%;
+    padding-left: 5%;
+}
+</style>
