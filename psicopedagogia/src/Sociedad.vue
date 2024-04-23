fc6fed00c616604153637c62b74d620e5a45cdc5
inaDAGod <danialee14@gmail.com>
2024-04-23 15:28:28 -0400

    add 4 blques responsive y subtitulos

--- psicopedagogia/src/Sociedad.vue
+++ psicopedagogia/src/Sociedad.vue
@@ -1,17 +1,54 @@
 <template>
-    <h2 class="titleInves">Investigacion <br>Sociedad Científica Estudiantil INPSICOPEDIA</h2>
-</template>
-
-<script>
-
-</script>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Koulen&display=swap');
-.titleInves{
+    <div>
+      <h2 class="titleInves">Investigacion <br>Sociedad Científica Estudiantil INPSICOPEDIA</h2>
+      <div class="bloque-container">
+        <Bloque titulo="¿Quienes somos?" contenido="Contenido del bloque" fondo="#AAD6FB" />
+        <Bloque titulo="¿Desde cuando existimos?" contenido="Contenido del bloque" fondo="#FEF684" />
+        <Bloque titulo="¿Quienes forman parte?" contenido="Contenido del bloque" fondo="#DBEE96" />
+        <Bloque titulo="¿Como unirse?" contenido="Contenido del bloque" fondo="#FFCEE8" />
+      </div>
+      <h3 class="subtitulo">Acciones investigativas</h3>
+      <h3 class="subtitulo">La presente Investigación</h3>
+    </div>
+  </template>
+  
+  <script setup>
+  import Bloque from './components/ParrafoInvestigacion.vue';
+  </script>
+  
+  <style>
+  @import url('https://fonts.googleapis.com/css2?family=Koulen&display=swap');
+  
+  .titleInves {
     color: #FF7001;
     font-family: 'Koulen';
     font-size: 300%;
     padding-left: 5%;
-}
-</style>
+  }
+  
+  .bloque-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding-left: 3%;
+    padding-right: 3%;
+  }
+  .subtitulo{
+    color: #FF7001;
+    text-align: left;
+    padding-left: 3%;
+  }
+
+  @media (max-width: 768px) {
+    .bloque-container {
+      flex-direction: column;
+      align-items: center;
+    }
+    .titleInves {
+        font-size: 200%;
+        padding-left: 5%;
+    }
+  }
+
+  </style>
+  
