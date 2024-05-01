ef7a85fe7922fdd856de9a71a34ee8632178901f
inaDAGod <danialee14@gmail.com>
2024-05-01 15:34:52 -0400

    updte cards redes

--- psicopedagogia/src/views/Red.vue
+++ psicopedagogia/src/views/Red.vue
@@ -5,75 +5,120 @@
     </div>
     <div class="cards-redes">
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/src/assets/images/red1.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Conferencia Episcopal Boliviana (CEB)</h5>
-                <p class="card-text">Descripcion falfak afalkfsal</p>
-                <a href="#" class="btn btn-primary">Ir</a>
+                <div class="botonConteiner">
+                    <a href="#" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/src/assets/images/red2.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Conferencia Bolivian de Religiosas y Religiosos (CBR)</h5>
-                <p class="card-text">Descripcion falfak afalkfsal</p>
-                <a href="#" class="btn btn-primary">Ir</a>
+                <h5 class="card-title">Conferencia Boliviana de Religiosas y Religiosos (CBR)</h5>
+                <div class="botonConteiner">
+                    <a href="#" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/src/assets/images/red3.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Escuela de Cristo</h5>
-                <p class="card-text">Descripcion falfak afalkfsal</p>
-                <a href="#" class="btn btn-primary">Ir</a>
+                <div class="botonConteiner">
+                    <a href="#" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/src/assets/images/red4.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Fundación Arco Iris</h5>
-                <p class="card-text">Descripcion falfak afalkfsal</p>
-                <a href="#" class="btn btn-primary">Ir</a>
+                <div class="botonConteiner">
+                    <a href="#" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/src/assets/images/red5.webp" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Fundación Levántate Mujer</h5>
-                <p class="card-text">Descripcion falfak afalkfsal</p>
-                <a href="#" class="btn btn-primary">Ir</a>
+                <div class="botonConteiner">
+                    <a href="#" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/src/assets/images/red6.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Fundación Munasim Kullakita</h5>
-                <p class="card-text">Descripcion falfak afalkfsal</p>
-                <a href="#" class="btn btn-primary">Ir</a>
+                <div class="botonConteiner">
+                    <a href="#" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/src/assets/images/red7.png" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Comedor San Calixto</h5>
-                <p class="card-text">Descripcion falfak afalkfsal</p>
-                <a href="#" class="btn btn-primary">Ir</a>
+                <div class="botonConteiner">
+                    <a href="#" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/src/assets/images/red8.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Centro Waliña</h5>
-                <p class="card-text">Descripcion falfak afalkfsal</p>
-                <a href="#" class="btn btn-primary">Ir</a>
+                <div class="botonConteiner">
+                    <a href="#" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="/src/assets/images/red9.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Fe y alegría</h5>
-                <p class="card-text">Descripcion falfak afalkfsal</p>
-                <a href="#" class="btn btn-primary">Ir</a>
+                <div class="botonConteiner">
+                    <a href="#" class="btn btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
@@ -108,11 +153,30 @@
         justify-content: space-between; /* Distribuye los elementos uniformemente a lo largo del contenedor */
         margin: 5%; /* Agrega un margen exterior */
     }
-
+    .botonConteiner{
+        text-align: right;
+    }
     .card {
         flex: 0 0 calc(33.33% - 20px); /* Establece el ancho de cada tarjeta, con un margen entre ellas */
         margin-bottom: 5%; /* Espacio entre las filas */
         box-sizing: border-box; /* Incluye el borde y el relleno en el tamaño total */
+    }
+    .cards-redes img{
+        height: 350px;
+        width: auto;
+        object-fit: cover; 
+    }
+    .cards-redes .card-body{
+        background-color: #DBEE96;
+    }
+    .cards-redes .btn {
+        background-color: #FF7001;
+        border: #FF7001;
+        align-self: flex-end; 
+        margin-top: auto;
+    }
+    .cards-redes .card-title{
+        height: 50%;
     }
 
 </style>
