af8975e8e8a2ffeab43325c8dc15ca2c7c0592f9
inaDAGod <danialee14@gmail.com>
2024-05-01 14:45:01 -0400

    add red.vue

---
+++ psicopedagogia/src/views/Red.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="tituloRed">
+        <h1>Red Psicopedagogia</h1>
+        <h2>Obras Sociales de la Iglesia</h2>   
+    </div>
+    
+</template>
+
+<style>
+    .tituloRed{
+        margin-left: 4%;
+        text-align: left;
+        padding: 2%;
+    }
+    .tituloRed h1{
+        color: #BADF3A;
+        font-size: 70px;
+        font-family: 'Koulen';
+        text-align: left;
+        padding-left: 0;
+        margin-left: 0%;
+    }
+    .tituloRed h2{
+        color: #FF7001;
+        font-size: 50px;
+        font-family: 'Koulen';
+        text-align: left;
+        padding-left: 0;
+        margin-left: 0%;
+    }
+</style>
