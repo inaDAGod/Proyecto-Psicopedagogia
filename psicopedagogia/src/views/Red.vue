db4756106682174d111b1daccebc0f05b353f5e4
inaDAGod <danialee14@gmail.com>
2024-05-01 15:03:17 -0400

    add cards red psico base

--- psicopedagogia/src/views/Red.vue
+++ psicopedagogia/src/views/Red.vue
@@ -3,6 +3,80 @@
         <h1>Red Psicopedagogia</h1>
         <h2>Obras Sociales de la Iglesia</h2>   
     </div>
+    <div class="cards-redes">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Conferencia Episcopal Boliviana (CEB)</h5>
+                <p class="card-text">Descripcion falfak afalkfsal</p>
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Conferencia Bolivian de Religiosas y Religiosos (CBR)</h5>
+                <p class="card-text">Descripcion falfak afalkfsal</p>
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Escuela de Cristo</h5>
+                <p class="card-text">Descripcion falfak afalkfsal</p>
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Fundación Arco Iris</h5>
+                <p class="card-text">Descripcion falfak afalkfsal</p>
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Fundación Levántate Mujer</h5>
+                <p class="card-text">Descripcion falfak afalkfsal</p>
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Fundación Munasim Kullakita</h5>
+                <p class="card-text">Descripcion falfak afalkfsal</p>
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Comedor San Calixto</h5>
+                <p class="card-text">Descripcion falfak afalkfsal</p>
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Centro Waliña</h5>
+                <p class="card-text">Descripcion falfak afalkfsal</p>
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Fe y alegría</h5>
+                <p class="card-text">Descripcion falfak afalkfsal</p>
+                <a href="#" class="btn btn-primary">Ir</a>
+            </div>
+        </div>
+    </div>
     
 </template>
 
@@ -28,4 +102,17 @@
         padding-left: 0;
         margin-left: 0%;
     }
+    .cards-redes {
+        display: flex;
+        flex-wrap: wrap; /* Permite que los elementos se envuelvan en filas */
+        justify-content: space-between; /* Distribuye los elementos uniformemente a lo largo del contenedor */
+        margin: 5%; /* Agrega un margen exterior */
+    }
+
+    .card {
+        flex: 0 0 calc(33.33% - 20px); /* Establece el ancho de cada tarjeta, con un margen entre ellas */
+        margin-bottom: 5%; /* Espacio entre las filas */
+        box-sizing: border-box; /* Incluye el borde y el relleno en el tamaño total */
+    }
+
 </style>
