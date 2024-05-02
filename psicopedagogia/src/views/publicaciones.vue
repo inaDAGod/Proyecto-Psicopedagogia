5dff574710f513877165543b4087d63a75a256be
inaDAGod <danialee14@gmail.com>
2024-05-01 23:04:45 -0400

    add buscador publicaciones

--- psicopedagogia/src/views/publicaciones.vue
+++ psicopedagogia/src/views/publicaciones.vue
@@ -2,12 +2,20 @@
   <div>
     <div class="content">
       <div class="titulo-publicacion">
-        <br><br><br>
-        <h1>PUBLICACIONES Y LIBROS</h1>
         <br><br>
+        <h1>PUBLICACIONES Y LIBROS</h1>
+      </div>
+      <div>
+        <input type="text" class="busquedaPubli">
+        <button class="buscarPubli">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+          </svg>
+        </button>
       </div>
     </div>
     <br>
+    
     <table class="container-publicaciones">
       <tbody>
         <tr v-for="(publicacionRow, rowIndex) in publicacionesRows" :key="rowIndex">
@@ -89,10 +97,11 @@ onMounted(() => {
   }
   .info-publicacion p {
     color: black;
-    text-align: center;
+    text-align: left;
     align-items: center;
     font-size: 3vh;
     font-family: 'Roboto Condensed', sans-serif;
+    
   }
   .info-publicacion h1 {
     color: black;
@@ -111,6 +120,28 @@ onMounted(() => {
     display: flex;
     margin-top: 3vh;
     margin-bottom: 3vh;
+    padding: 1%;
+    margin: 2%;
+  }
+  .busquedaPubli{
+    width: 60%;
+    margin: 2%;
+    padding: 1%;
+    margin-left: 5%;
+    border-radius: 40px;
+    border-color: #ff710441;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5); 
+  }
+  .buscarPubli{
+    background-color: #BADF3A;
+    border-color: #BADF3A;
+    padding: 1%;
+    border-radius: 40px;
+    box-shadow: 0px 0px 10px rgba(82, 218, 89, 0.452); 
+  }
+  .buscarPubli:hover{
+    background-color: #FF7001;
+    border-color: #FF7001;
   }
   .publicacion-imagen{
     flex:1;
