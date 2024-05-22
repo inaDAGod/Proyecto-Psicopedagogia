3f938abe7047fbdaba7eed4bfdeb6d539a73ebb6
unknown <ayana.siegle@ucb.edu.bo>
2024-05-22 04:09:26 -0400

    Revert "for no"
    This reverts commit 6c0ba4e5b2a404dae88b4777dc92eb9ba6b5a745.

--- psicopedagogia/src/views/Nosotrosadmin.vue
+++ psicopedagogia/src/views/Nosotrosadmin.vue
@@ -1,174 +1,226 @@
 <template>
-  <div>
-    <div v-if="notification" class="notification">
-      {{ notification }}
+    <div id="app">
+      <!-- First section: NOSOTROS -->
+      <div style="display: flex; align-items: center;">
+        <h1 class="titulo-nos">NOSOTROS</h1>
+        <div style="display: flex; align-items: center; justify-content: flex-end; flex-grow: 1;">
+          <div style="margin: 20px;">
+            <!-- Button to toggle the Formpaginanos component -->
+            <button class="b-formna" @click="toggleFormVisibility('formPaginanos')">Modificar</button>
+            <!-- Formpaginanos component -->
+            <Formpaginanos v-if="showFormPaginanos" @closeForm="closeForm('formPaginanos')" />
+          </div>
+        </div>
+      </div>
+  
+      <!-- Video section -->
+      <div class="video-container">
+        <!-- Paste the iframe code here -->
+        <iframe class="video-nos" :src="getEmbeddedLink(paginaNosotros.link_video)" frameborder="0" allowfullscreen></iframe>
+      </div>
+  
+      <!-- Second section: NUESTRA FAMILIA -->
+      
+  
+      <!-- Displaying PROGRAMAS -->
+      <h1 class="titulo-nos">PROGRAMAS</h1>
+      <div style="text-align: center; margin-bottom: 5%">
+        <a :href="paginaNosotros.link_soc_cien"><button class="b-linksna">Sociedad Científica Estudiantil Inpsicopedia</button></a><br>
+        <a :href="paginaNosotros.link_sembrando"><button class="b-linksna">Programa “Sembrando Semillas de Paz”</button></a><br>
+        <a :href="paginaNosotros.link_psico_ucb"><button class="b-linksna">Psicopedagogía La Paz</button></a><br><br><br>
+      </div>
+      
+  
+      <!-- Footer -->
+      <footer>
+        <img src="/src/assets/images/rocas.png" alt="Image Description" class="afooternos-img">
+        <div class="afooternos-container">
+          <table class="afooternos-table">
+      <tr class="afooternos-tr">
+        <td class="afooternos-td">
+          <a :href="paginaNosotros.facebook"><img class="imgnosa" src="/src/assets/images/facebook.png" alt="Imagen del logo"></a>
+        </td>
+        <td class="afooternos-td">
+          <p class="afooternos-p">Psicopedagogía UCB La Paz</p>
+        </td>
+      </tr>
+      <tr class="afooternos-tr">
+        <td class="afooternos-td">
+          <a :href="paginaNosotros.insta"><img class="imgnosa"   src="/src/assets/images/insta.png" alt="Imagen del logo"></a>
+        </td>
+        <td class="afooternos-td">
+          <p class="afooternos-p">ucb.psp</p>
+        </td>
+      </tr>
+      <tr class="afooternos-tr">
+        <td class="afooteros-td">
+          <a :href="paginaNosotros.youtube"><img class="imgnosa"  src="/src/assets/images/yt.png" alt="Imagen del logo"></a>
+        </td>
+        <td class="afooternos-td">
+          <p class="afooternos-p">canal de youtube</p>
+        </td>
+      </tr>
+      <tr class="afooternos-tr">
+        <td class="afooternos-td">
+          <a :href="paginaNosotros.tiktok"><img class="imgnosa"  src="/src/assets/images/tiktok.png" alt="Imagen del logo"></a>
+        </td>
+        <td class="afooternos-td">
+          <p class="afooternos-p">psp.ucb.lapaz</p>
+        </td>
+      </tr>
+    </table>
+  
+          <div class="afooternos-info">
+            <h2 class="afooternos-mainnos">Atención de Dirección de Carrera</h2>
+            <p id="direcion">{{ paginaNosotros.attencion_dire }}</p>
+          </div>
+        </div>
+      </footer>
     </div>
-    <div class="modal" v-show="showForm">
-      <div class="modal-content">
-        <form @submit.prevent="submitForm">
-          <!-- Input fields for the form -->
-          <div class="form-group">
-            <label for="link_video">Link del Video:</label><br>
-            <input type="text" id="link_video" v-model="linkVideo" required>
-          </div>
-          <div class="form-group">
-            <label for="link_soc_cien">Link Soc. Científica:</label><br>
-            <input type="text" id="link_soc_cien" v-model="linkSocCien" required>
-          </div>
-          <div class="form-group">
-            <label for="link_sembrando">Link Sembrando:</label><br>
-            <input type="text" id="link_sembrando" v-model="linkSembrando" required>
-          </div>
-          <div class="form-group">
-            <label for="link_psico_ucb">Link Psico UCB:</label><br>
-            <input type="text" id="link_psico_ucb" v-model="linkPsicoUcb" required>
-          </div>
-          <h1>Redes:</h1>
-          <div class="form-group">
-            <img style="width: 12%;" src="/src/assets/images/facebook.png" alt="Imagen del logo">
-            <input type="text" id="facebook" v-model="facebook" required>
-          </div>
-          <div class="form-group">
-            <img style="width: 12%;" src="/src/assets/images/insta.png" alt="Imagen del logo">
-            <input type="text" id="insta" v-model="insta" required>
-          </div>
-          <div class="form-group">
-            <img style="width: 12%;height: 20%;" src="/src/assets/images/yt.png" alt="Imagen del logo">
-            <input type="text" id="youtube" v-model="youtube" required>
-          </div>
-          <div class="form-group">
-            <img style="width: 12%;" src="/src/assets/images/tiktok.png" alt="Imagen del logo">
-            <input type="text" id="tiktok" v-model="tiktok" required>
-          </div>
-          <div class="form-group">
-            <label style="margin-left: 2.5%;" for="attencion_dire">Atención Dirección:</label><br>
-            <textarea style="width: 175%;" id="attencion_dire" v-model="attencionDire" rows="4" required></textarea>
-          </div>
-          <div style="text-align: center;">
-            <!-- Submit button -->
-            <button class="boton-guardar">Guardar</button>
-          </div>
-        </form>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
+  import Formpaginanos from '/src/Formp.vue';
+  import FormDocente from '/src/FormDocente.vue';
+  import FormDocMod from '/src/FormDocMod.vue';
+  import FormDocElim from '/src/FormDocElim.vue';
+  
+  // Define separate reactive variables for each form FormDocElim
+  const showFormPaginanos = ref(false);
+  const showFormDocente = ref(false);
+  const showFormDocMod = ref(false);
+  const showFormDocElim = ref(false);
+  
+  // Function to toggle the visibility of each form based on its name
+  const toggleFormVisibility = (formName) => {
+    if (formName === 'formPaginanos') {
+      showFormPaginanos.value = !showFormPaginanos.value;
+    } else if (formName === 'formDocente') {
+      showFormDocente.value = !showFormDocente.value;
+    } else if (formName === 'formDocMod') {
+      showFormDocMod.value = !showFormDocMod.value;
+    } else if (formName === 'formDocElim') {
+      showFormDocElim.value = !showFormDocElim.value;
+    }
+  };
+  
+  // Function to close each form based on its name
+  const closeForm = (formName) => {
+    if (formName === 'formPaginanos') {
+      showFormPaginanos.value = false;
+    } else if (formName === 'formDocente') {
+      showFormDocente.value = false;
+    }
+    else if (formName === 'formDocMod') {
+      showFormDocMod.value = false;
+    }
+    else if (formName === 'formDocElim') {
+      showFormDocElim.value = false;
+    }
+  };
+  
+  const getEmbeddedLink = (link) => {
+  // Verifica si el enlace es del formato "watch" y lo convierte a "embed"
+  if (link.includes('youtube.com/watch')) {
+    const videoId = link.split('v=')[1];
+    return `https://www.youtube.com/embed/${videoId}`;
+  }
+  // Si el enlace ya está en formato "embed", devuelve el mismo enlace
+  return link;
+};
+
+
+
+  // Define reactive variables to store data for docentes and paginaNosotros
+  const docentes = ref([]);
+  const paginaNosotros = ref({
+    link_video: '',
+    link_soc_cien: '',
+    link_sembrando: '',
+    link_psico_ucb: '',
+    facebook: '',
+    insta: '',
+    youtube: '',
+    tiktok: '',
+    attencion_dire: ''
+  });
+  
+  // Function to fetch docentes data from the database
+  const obtenerDocentes = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/docentes');
+      docentes.value = response.data;
+    } catch (error) {
+      console.error('Error fetching docentes:', error);
+    }
+  };
+  
+  // Function to fetch paginaNosotros data from the database
+  const obtenerInfoPagina = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/info-pagina');
+      paginaNosotros.value = response.data;
+    } catch (error) {
+      console.error('Error fetching paginaNosotros data:', error);
+    }
+  };
+  
+  // Call the functions to fetch data when the component is mounted
+  onMounted(() => {
+    obtenerDocentes();
+    obtenerInfoPagina();
+  });
+  
+  // Function to generate HTML table for docentes
+  const generateTable = (docente) => {
+  const tableHTML = `<center>
+    <div class="docentesna">
+      <div class="docentes-infona">
+        <p ><b> ${docente.nombre}</b><br><br>
+         ${docente.cargo}<br>
+         ${docente.correo}<br>
+         Dato curioso:
+         ${docente.datoc}</p>
+      </div>
+      <div class="docentes-avatarna">
+        <div class="docentes-nicknamena">${docente.apodo}</div>
+        <img src="${docente.imagen}" alt="${docente.nombre}" class="docentes-imagena">
       </div>
     </div>
-  </div>
-</template>
+  <center>`;
 
-
-<script setup>
-import { ref, defineProps, watch } from 'vue';
-
-const showForm = ref(true);
-const notification = ref(null);
-const props = defineProps({
-  linkVideo: String,
-  linkSocCien: String,
-  linkSembrando: String,
-  linkPsicoUcb: String,
-  facebook: String,
-  insta: String,
-  youtube: String,
-  tiktok: String,
-  attencionDire: String,
-  onclose: Function
-});
-
-const linkVideo = ref(props.linkVideo);
-const linkSocCien = ref(props.linkSocCien);
-const linkSembrando = ref(props.linkSembrando);
-const linkPsicoUcb = ref(props.linkPsicoUcb);
-const facebook = ref(props.facebook);
-const insta = ref(props.insta);
-const youtube = ref(props.youtube);
-const tiktok = ref(props.tiktok);
-const attencionDire = ref(props.attencionDire);
-
-// Function to fetch data from the database
-const fetchData = async () => {
-  try {
-    const response = await fetch('http://localhost:3000/api/info-pagina');
-    if (response.ok) {
-      const data = await response.json();
-      console.log('Fetched data:', data);
-      // Update variables with fetched data
-      linkVideo.value = data.linkVideo;
-      linkSocCien.value = data.linkSocCien;
-      linkSembrando.value = data.linkSembrando;
-      linkPsicoUcb.value = data.linkPsicoUcb;
-      facebook.value = data.facebook;
-      insta.value = data.insta;
-      youtube.value = data.youtube;
-      tiktok.value = data.tiktok;
-      attencionDire.value = data.attencionDire;
-    } else {
-      console.error('Error fetching data:', response.statusText);
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
+  return tableHTML;
 };
+  </script>
+  
+ 
+  
+  
+  
+  
 
-// Watch for changes in props and fetch data accordingly
-watch(
-  () => [props.linkVideo, props.linkSocCien, props.linkSembrando, props.linkPsicoUcb, props.facebook, props.insta, props.youtube, props.tiktok, props.attencionDire],
-  fetchData,
-  { deep: true }
-);
 
-// Function to send the form data to the server
-const submitForm = async () => {
-  try {
-    const formData = new FormData(); // Create FormData object to send data to the server
-    formData.append('link_video', linkVideo.value);
-    formData.append('link_soc_cien', linkSocCien.value);
-    formData.append('link_sembrando', linkSembrando.value);
-    formData.append('link_psico_ucb', linkPsicoUcb.value);
-    formData.append('facebook', facebook.value);
-    formData.append('insta', insta.value);
-    formData.append('youtube', youtube.value);
-    formData.append('tiktok', tiktok.value);
-    formData.append('attencion_dire', attencionDire.value);
 
-    // Send the form data to the server
-    const response = await fetch('http://localhost:3000/api/info-pagina', {
-      method: 'POST',
-      body: formData 
-    });
-    
-    if (response.ok) {
-      console.log('Data updated successfully');
-      notification.value = 'Actualización exitosa';
-      setTimeout(() => {
-        notification.value = null;
-      }, 3000);
-    } else {
-      console.error('Error updating data:', response.statusText);
-    }
-  } catch (error) {
-    console.error('Error submitting form:', error);
-  }
-};
-</script>
 
-<style scoped>
-@import url('/src/assets/formPaginanos.css');
 
-/* Notification styles */
-.notification {
-  background-color: #4CAF50; /* Green */
-  color: white;
-  padding: 15px;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 1000;
-  border-radius: 5px;
-  text-align: center;
-  margin-left: 8%;
-}
 
-/* Other styles here */
+
+
+
+
+
+
+
+
+
+  <style>
+@import url('/src/assets/nostrosadmin.css');
+
+
+
+
 </style>
 
 
