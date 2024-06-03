d18bdfda538572e5141947d77f2579b891e68ee1
Jhuly1215 <jhuliannnavitoria@gmail.com>
2024-06-03 00:04:14 -0400

    interaccion social responsive

--- psicopedagogia/src/views/social.vue
+++ psicopedagogia/src/views/social.vue
@@ -8,119 +8,67 @@
       </div>
     </div>
     <div class="container-objetivo">
-        <div class="text-box-objetivo">
-          <div class="text-content-objetivo">
-            <h1>OBJETIVOS</h1>
-            <p>{{ paginaInteraccion.objetivo }}</p>
-          </div>
+      <div class="text-box-objetivo">
+        <div class="text-content-objetivo">
+          <h1>OBJETIVOS</h1>
+          <p>{{ paginaInteraccion.objetivo }}</p>
         </div>
+      </div>
     </div>
     <div class="container-lineas">
       <div class="text-box-lineas">
-          <div class="text-content-lineas">
-            <h1>LINEAS DE ACCIÓN MACRO</h1>
-            <p>{{ paginaInteraccion.lineas }}</p>
-          </div>
+        <div class="text-content-lineas">
+          <h1>LINEAS DE ACCIÓN MACRO</h1>
+          <p>{{ paginaInteraccion.lineas }}</p>
         </div>
+      </div>
     </div>
-    <div class="vacio">
-      <br>
-    </div>
+    <br>
     <div class="subtitulo-actividad">
       <h1>ACTIVIDADES UNIVERSITARIAS</h1>
     </div>
     <br>
-    <table class="container-actividades">
-      <tbody>
-        <tr v-for="(actividadRow, rowIndex) in actividadURows" :key="rowIndex">
-          <td v-for="(actividadU, index) in actividadRow" :key="index">
-            <div class="actividades">
-              <div class="actividad-imagen">
-                <img :src="actividadU.actividad_src" :alt="actividadU.actividad">
-              </div>
-              <div class="actividad-nombre">
-                <button class="btn-actividad-universidad" @click="toggleInfoActividadU(rowIndex, index)">{{ actividadU.actividad }}</button>
-                <div v-if="expandedInfoActividadU[rowIndex][index]" class="info-actividad-universidad">
-                  <p>Fecha: {{ actividadU.fecha }}</p>
-                  <p>Hora: {{ actividadU.hora }}</p>
-                  <p>Ubicación: {{ actividadU.ubicacion }}</p>
-                  <p>Descripción: {{ actividadU.descripcion }}</p>
-                </div>
-              </div>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container-actividades row">
+      <div v-for="(actividad, index) in actividadesU" :key="index" class="col-lg-4 col-md-6 col-sm-12 mb-3">
+        <CardActividad :actividad="actividad" tipo="universidad" />
+      </div>
+    </div>
 
     <!-- Actividades Departamentales -->
     <div class="subtitulo-actividad">
       <h1>ACTIVIDADES DEPARTAMENTALES</h1>
     </div>
     <br>
-    <table class="container-actividades">
-      <tbody>
-        <tr v-for="(actividadRow, rowIndex) in actividadDRows" :key="rowIndex">
-          <td v-for="(actividadD, index) in actividadRow" :key="index">
-            <div class="actividades">
-              <div class="actividad-imagen">
-                <img :src="actividadD.actividad_src" :alt="actividadD.actividad">
-              </div>
-              <div class="actividad-nombre">
-                <button class="btn-actividad-departamental" @click="toggleInfoActividadD(rowIndex, index)">{{ actividadD.actividad }}</button>
-                <div v-if="expandedInfoActividadD[rowIndex][index]" class="info-actividad-departamental">
-                  <p>Fecha: {{ actividadD.fecha }}</p>
-                  <p>Hora: {{ actividadD.hora }}</p>
-                  <p>Ubicación: {{ actividadD.ubicacion }}</p>
-                  <p>Descripción: {{ actividadD.descripcion }}</p>
-                </div>
-              </div>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container-actividades row">
+      <div v-for="(actividad, index) in actividadesD" :key="index" class="col-lg-4 col-md-6 col-sm-12 mb-3">
+        <CardActividad :actividad="actividad" tipo="departamental" />
+      </div>
+    </div>
 
     <!-- Actividades Internacionales -->
     <div class="subtitulo-actividad">
       <h1>ACTIVIDADES INTERNACIONALES</h1>
     </div>
     <br>
-    <table class="container-actividades">
-      <tbody>
-        <tr v-for="(actividadRow, rowIndex) in actividadIRows" :key="rowIndex">
-          <td v-for="(actividadI, index) in actividadRow" :key="index">
-            <div class="actividades">
-              <div class="actividad-imagen">
-                <img :src="actividadI.actividad_src" :alt="actividadI.actividad">
-              </div>
-              <div class="actividad-nombre">
-                <button class="btn-actividad-internacional"  @click="toggleInfoActividadI(rowIndex, index)">{{ actividadI.actividad }}</button>
-                <div v-if="expandedInfoActividadI[rowIndex][index]" class="info-actividad-internacional">
-                  <p>Fecha: {{ actividadI.fecha }}</p>
-                  <p>Hora: {{ actividadI.hora }}</p>
-                  <p>Ubicación: {{ actividadI.ubicacion }}</p>
-                  <p>Descripción: {{ actividadI.descripcion }}</p>
-                </div>
-              </div>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container-actividades row">
+      <div v-for="(actividad, index) in actividadesI" :key="index" class="col-lg-4 col-md-6 col-sm-12 mb-3">
+        <CardActividad :actividad="actividad" tipo="internacional" />
+      </div>
+    </div>
   </div>
 </template>
-  
+
 <script setup>
-
-import {ref, onMounted} from 'vue';
+import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import CardActividad from '/src/components/CardActividad.vue';
 
-// Define the reactive variables to home
+// Define the reactive variables for home
 const paginaInteraccion = ref({
-    objetivo: '',
-    lineas: '',
+  objetivo: '',
+  lineas: '',
 });
+
 // Function to fetch home data from the database
 const obtenerInteraccion = async () => {
   try {
@@ -134,126 +82,194 @@ onMounted(() => {
   obtenerInteraccion();
 });
 
-  const actividadesU = ref([]);
-  const actividadURows = ref([]);
-  const expandedInfoActividadU = ref([]);
-  
-  const obtenerActividades_Universidad = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000/api/actividades_universidad');
-      actividadesU.value = response.data;
-      calcularFilasDeActividadesU();
-    } catch (error) {
-      console.error('Error fetching Actividades:', error);
-    }
-  };
-  
-  const calcularFilasDeActividadesU = () => {
-    actividadURows.value = [];
-    expandedInfoActividadU.value = [];
-    for (let i = 0; i < actividadesU.value.length; i += 3) {
-      const row = [];
-      const infoRow = [];
-      for (let j = i; j < i + 3 && j < actividadesU.value.length; j++) {
-        row.push(actividadesU.value[j]);
-        infoRow.push(false); // Inicialmente la información está oculta
-      }
-      actividadURows.value.push(row);
-      expandedInfoActividadU.value.push(infoRow);
-    }
-  };
-  
-  const toggleInfoActividadU = (rowIndex, index) => {
-    expandedInfoActividadU.value[rowIndex][index] = !expandedInfoActividadU.value[rowIndex][index];
-  };
-  
-  onMounted(() => {
-    obtenerActividades_Universidad();
-  });
+// Actividades Universitarias
+const actividadesU = ref([]);
+const obtenerActividades_Universidad = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/api/actividades_universidad');
+    actividadesU.value = response.data;
+  } catch (error) {
+    console.error('Error fetching Actividades:', error);
+  }
+};
+onMounted(() => {
+  obtenerActividades_Universidad();
+});
 
-  
-  const actividadesD = ref([]);
-  const actividadDRows = ref([]);
-  const expandedInfoActividadD = ref([]);
-  
-  const obtenerActividades_Departamental = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000/api/actividades_departamental');
-      actividadesD.value = response.data;
-      calcularFilasDeActividadesD();
-    } catch (error) {
-      console.error('Error fetching Actividades:', error);
-    }
-  };
-  
-  const calcularFilasDeActividadesD = () => {
-    actividadDRows.value = [];
-    expandedInfoActividadD.value = [];
-    for (let i = 0; i < actividadesD.value.length; i += 3) {
-      const row = [];
-      const infoRow = [];
-      for (let j = i; j < i + 3 && j < actividadesD.value.length; j++) {
-        row.push(actividadesD.value[j]);
-        infoRow.push(false); // Inicialmente la información está oculta
-      }
-      actividadDRows.value.push(row);
-      expandedInfoActividadD.value.push(infoRow);
-    }
-  };
-  
-  const toggleInfoActividadD = (rowIndex, index) => {
-    expandedInfoActividadD.value[rowIndex][index] = !expandedInfoActividadD.value[rowIndex][index];
-  };
-  
-  onMounted(() => {
-    obtenerActividades_Departamental();
-  });
+// Actividades Departamentales
+const actividadesD = ref([]);
+const obtenerActividades_Departamental = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/api/actividades_departamental');
+    actividadesD.value = response.data;
+  } catch (error) {
+    console.error('Error fetching Actividades:', error);
+  }
+};
+onMounted(() => {
+  obtenerActividades_Departamental();
+});
 
-  const actividadesI = ref([]);
-  const actividadIRows = ref([]);
-  const expandedInfoActividadI = ref([]);
-  
-  const obtenerActividades_Internacional = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000/api/actividades_internacional');
-      actividadesI.value = response.data;
-      calcularFilasDeActividadesI();
-    } catch (error) {
-      console.error('Error fetching Actividades:', error);
-    }
-  };
-  
-  const calcularFilasDeActividadesI = () => {
-    actividadIRows.value = [];
-    expandedInfoActividadI.value = [];
-    for (let i = 0; i < actividadesI.value.length; i += 3) {
-      const row = [];
-      const infoRow = [];
-      for (let j = i; j < i + 3 && j < actividadesI.value.length; j++) {
-        row.push(actividadesI.value[j]);
-        infoRow.push(false); // Inicialmente la información está oculta
-      }
-      actividadIRows.value.push(row);
-      expandedInfoActividadI.value.push(infoRow);
-    }
-  };
-  
-  const toggleInfoActividadI = (rowIndex, index) => {
-    expandedInfoActividadI.value[rowIndex][index] = !expandedInfoActividadI.value[rowIndex][index];
-  };
-  
-  onMounted(() => {
-    obtenerActividades_Internacional();
-  });
+// Actividades Internacionales
+const actividadesI = ref([]);
+const obtenerActividades_Internacional = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/api/actividades_internacional');
+    actividadesI.value = response.data;
+  } catch (error) {
+    console.error('Error fetching Actividades:', error);
+  }
+};
+onMounted(() => {
+  obtenerActividades_Internacional();
+});
 </script>
-  
-<style scoped>
-  @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Koulen&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
-  @import url('/src/assets/estilo.css');
-  @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
-  @import url('/src/assets/interaccion.css');
-  
 
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Koulen&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
+@import url('/src/assets/estilo.css');
+@import url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
+
+.text-box-objetivo {
+  background-color: #ffc497; /* Color de fondo para el text box de objetivo */
+  text-align: center; /* Centra el texto */
+  color: black; /* Color del texto */
+  border-radius: 5vh;
+  padding: 7vh;
+  margin-bottom: 5%; /* Espacio entre el recuadro y el carrusel */
+  max-width: 90%;
+}
+
+.text-box-lineas {
+  background-color: #ffcee8; /* Color de fondo para el text box de líneas */
+  text-align: center; /* Centra el texto */
+  color: black; /* Color del texto */
+  border-radius: 5vh;
+  padding: 7vh;
+  margin-bottom: 5%; /* Espacio entre el recuadro y el carrusel */
+  max-width: 90%;
+}
+
+.container-objetivo,
+.container-lineas {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.text-box-objetivo p,
+.text-box-lineas p {
+  padding-top: 3vh;
+  padding-left: 3vh;
+  padding-right: 3vh;
+  padding-bottom: 3vh;
+  font-family: 'Roboto Condensed', sans-serif;
+  text-align: center;
+  font-size: 3.5vh;
+}
+
+.text-box-objetivo h1,
+.text-box-lineas h1 {
+  color: black; /* Color del texto */
+  font-size: 4vh;
+}
+
+.titulo-social h1 {
+  margin-left: 5vh;
+  font-family: 'Oswald', 'sans-serif';
+  font-size: 5vh;
+  color: #ff7001;
+}
+
+.container-actividades {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.subtitulo-actividad h1{
+    font-size: 7vh;
+    color: #FF7001;
+    font-family: 'Koulen', 'sans-serif';
+    margin-top: 4vh;
+}
+@media (max-width: 992px) {
+  .container-objetivo,
+  .container-lineas{
+    width:100%;
+  }
+  .text-box-objetivo,
+  .text-box-lineas {
+    padding: 5vh;
+    margin-bottom: 3%;
+    width:100%;
+  }
+
+  .text-box-objetivo p,
+  .text-box-lineas p {
+    font-size: 3vh;
+  }
+
+  .text-box-objetivo h1,
+  .text-box-lineas h1 {
+    font-size: 3.5vh;
+  }
+  .subtitulo-actividad h1{
+      font-size: 3.7vh;
+      color: #FF7001;
+      font-family: 'Koulen', 'sans-serif';
+      margin-top: 4vh;
+    }
+}
+
+@media (max-width: 768px) {
+  .text-box-objetivo,
+  .text-box-lineas {
+    padding: 3vh;
+    margin-bottom: 2%;
+    width:100%;
+  }
+
+  .text-box-objetivo p,
+  .text-box-lineas p {
+    font-size: 2.5vh;
+  }
+
+  .text-box-objetivo h1,
+  .text-box-lineas h1 {
+    font-size: 3vh;
+  }
+  .subtitulo-actividad h1{
+      font-size: 2.7vh;
+      color: #FF7001;
+      font-family: 'Koulen', 'sans-serif';
+      margin-top: 4vh;
+  }
+}
+
+@media (max-width: 576px) {
+  .text-box-objetivo,
+  .text-box-lineas {
+    padding: 2vh;
+    margin-bottom: 1%;
+    width:100%;
+  }
+
+  .text-box-objetivo p,
+  .text-box-lineas p {
+    font-size: 2vh;
+  }
+
+  .text-box-objetivo h1,
+  .text-box-lineas h1 {
+    font-size: 2.5vh;
+  }
+  .subtitulo-actividad h1{
+      font-size: 2.7vh;
+      color: #FF7001;
+      font-family: 'Koulen', 'sans-serif';
+      margin-top: 4vh;
+  }
+}
 </style>
