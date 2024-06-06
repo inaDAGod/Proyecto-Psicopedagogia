c73a1846e3b24e6c57df99ad954b116ee9ab4c9f
inaDAGod <danialee14@gmail.com>
2024-06-06 01:08:06 -0400

    change admins styles

--- psicopedagogia/src/main.js
+++ psicopedagogia/src/main.js
@@ -4,5 +4,5 @@ import App2 from './App2.vue';
 import router from '/src/router/index.js'; 
 import router2 from '/src/router/index2.js'; 
 
-createApp(App).use(router).mount('#app');  //vista normal
-//createApp(App2).use(router2).mount('#app');  //vista administrador
+//createApp(App).use(router).mount('#app');  //vista normal
+createApp(App2).use(router2).mount('#app');  //vista administrador
